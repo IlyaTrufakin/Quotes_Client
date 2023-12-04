dc3495965dfaf22d5b095dbffb488f728536b1a8
Ilya Trufakin <trufakin@hotmail.com>
2023-12-04 22:55:24 +0200

    ver0.1

---
+++ ClientCommunication.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quotes_Client
+{
+    public class ClientCommunication
+    {
+        private Socket? clientSocket;
+
+        public string ConnectToServer(string ipAddress, string port)
+        {
+            try
+            {
+                if (IsConnected())
+                {
+                    return "Соединение с сервером уже установлено";
+                }
+                IPEndPoint endpoint = SocketInit(ipAddress, port);
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.Connect(endpoint);
+                return "Соединение с сервером установлено";
+            }
+            catch (Exception ex)
+            {
+                return "Ошибка соединения с сервером: " + ex.Message;
+            }
+        }
+
+        public bool IsConnected()
+        {
+            return clientSocket != null && clientSocket.Connected;
+        }
+
+
+        public async Task<string> SendMessageAndReceiveAsync(string message)
+        {
+            try
+            {
+                byte[] sendData = Encoding.Unicode.GetBytes(message);
+                await clientSocket.SendAsync(new ArraySegment<byte>(sendData), SocketFlags.None);
+
+                // Ожидание ответа в отдельном потоке
+                Task<string> receiveTask = Task.Run(async () =>
+                {
+                    byte[] receiveData = new byte[256];
+                    StringBuilder receivedString = new StringBuilder();
+
+                    while (true)
+                    {
+                        int bytesReceived = await clientSocket.ReceiveAsync(new ArraySegment<byte>(receiveData), SocketFlags.None);
+                        if (clientSocket.Available > 0)
+                        {
+                            receivedString.Append(Encoding.Unicode.GetString(receiveData, 0, bytesReceived));
+                        }
+                        else
+                        {
+                            break; // Если не получено байтов, завершаем цикл
+                        }
+                    }
+
+                    return receivedString.ToString();
+                });
+
+                // Дождитесь завершения задачи по получению ответа
+                string receivedMessage = await receiveTask;
+
+                return receivedMessage;
+            }
+            catch (Exception ex)
+            {
+                return "Ошибка отправки сообщения/получения ответа: " + ex.Message;
+            }
+        }
+
+
+
+        public string SendMessage(string message)
+        {
+            try
+            {
+                byte[] sendData = Encoding.Unicode.GetBytes(message);
+                clientSocket.Send(sendData);
+
+                byte[] receiveData = new byte[256];
+                StringBuilder receivedString = new StringBuilder();
+                int bytesReceived;
+
+                while (true)
+                {
+                    bytesReceived = clientSocket.Receive(receiveData);
+                    receivedString.Append(Encoding.Unicode.GetString(receiveData, 0, bytesReceived));
+
+                    if (bytesReceived < receiveData.Length)
+                    {
+                        break;
+                    }
+                }
+
+                return receivedString.ToString();
+            }
+            catch (Exception ex)
+            {
+                return "Ошибка отправки сообщения/получения ответа: " + ex.Message;
+            }
+        }
+
+        private IPEndPoint SocketInit(string ipAddress, string port)
+        {
+            try
+            {
+                if (!int.TryParse(port, out int portNumber))
+                {
+                    throw new ArgumentException("Порт задан неверно (должен быть числом)");
+                }
+
+                return new IPEndPoint(IPAddress.Parse(ipAddress), portNumber);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка инициализации сокета: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Quotes_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        private DispatcherTimer timer;
+        private ClientCommunication clientCommunication;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            clientCommunication = new ClientCommunication();
+            // Создаем таймер и устанавливаем интервал на 1 секунду
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            /*         if (serverCommunication.IsConnected())
+                     {
+                         string response = serverCommunication.SendMessage("timeQuiet");
+                         Status1.Text = "Время Сервера: " + response;
+                         Status2.Text = "Соединение с сервером: установлено";
+                     }
+                     else
+                     {
+                         Status1.Text = "Время Сервера: не доступно";
+                         Status2.Text = "Соединение с сервером: не установлено";
+                     }*/
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionResult = clientCommunication.ConnectToServer(ipAddress.Text, portNumber.Text);
+            OutputWindow.Text += connectionResult + Environment.NewLine;
+            SendMessageButton.IsEnabled = clientCommunication.IsConnected();
+            ScrollTextBlock.ScrollToEnd();
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SendMessageButton.IsEnabled = false;
+        }
+
+
+        private async Task<string> SendMessageToServer(string message)
+        {
+            try
+            {
+                if (clientCommunication.IsConnected())
+                {
+                    if (!string.IsNullOrEmpty(message))
+                    {
+                        string response = await clientCommunication.SendMessageAndReceiveAsync(message);
+                        return response; // Возвращаем полученный ответ
+                    }
+                }
+                else
+                {
+                    return "Сообщение не отправлено: нет соединения с сервером" + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Обработка исключения при возникновении ошибки в асинхронной операции
+                return "Ошибка отправки сообщения: " + ex.Message + Environment.NewLine;
+
+            }
+
+            return "Ошибка отправки сообщения:"; // Если что-то пошло не так, возвращаем пустую строку или можно выбрасывать исключение.
+        }
+
+
+
+
+        private async void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            string response = await SendMessageToServer(InputWindow.Text);
+            OutputWindow.Text += "Ответ сервера: " + response + Environment.NewLine;
+            ScrollTextBlock.ScrollToEnd();
+        }
+
+
+
+        private async void InputWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string response = await SendMessageToServer(InputWindow.Text);
+                OutputWindow.Text += "Ответ сервера: " + response + Environment.NewLine;
+                ScrollTextBlock.ScrollToEnd();
+            }
+        }
+    }
+}
